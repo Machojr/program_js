f4468c73d07e7d60fb8778bc89187b8085bc9fd9
Young Rich <tajiriramadhan@gmail.com>
2024-08-23 18:23:00 +0300

    accessing and displaying values to the display

--- calu2.js
+++ calu2.js
@@ -1,17 +1,52 @@
-const display = document.getElementById('display');
-const button1 = document.getElementById('buttons');
-function button() {
-    onclick = (display.value(button));
-    display.innerHTML = (`${button}`)
-}
-//display.innerHTML = document.getElementById()
-function solve(input){
-    display.value = eval(display.value);
-    display.innerHTML=
-}
-function clear() {
-    display.value = '';
-}
-function back() {
-    display.value= display.value.toString().slice(0, -1);
-}
+const display = document.getElementById('displayScreen');
+const btnSawasawa = document.getElementById('sawasawa');
+
+const kitufe = document.querySelectorAll('.kitufe');
+console.log(kitufe);
+
+kitufe.forEach(function (kitufe) {
+    console.log(kitufe);
+    console.log(kitufe.attributes.value.value);
+    kitufe.addEventListener('click', function () {
+        const kitufeValue = kitufe.attributes.value.value;
+        if (kitufeValue === 'C' || kitufeValue === 'AC') {
+            display.value = '';
+        } else if (kitufeValue === '=') {
+            display.value = eval(display.value);
+        } else {
+            display.value += kitufeValue;
+        }
+    });
+    });
+
+// function kitufeKimebonyezwa(e) {
+//     //const kitufe = e.target;
+//     console.log(e);
+//     const kitufeValue = kitufe.innerText;
+//     if (kitufeValue === 'C') {
+//         display.value = '';
+//     } else if (kitufeValue === '=') {
+//         display.value = eval(display.value);
+//     } else {
+//         display.value += kitufeValue;
+//     }
+// }
+
+
+
+
+// function button() {
+//     onclick = (display.value(button));
+//     display.innerHTML = (`${button}`)
+// }
+// //display.innerHTML = document.getElementById()
+// function solve(input) {
+//     display.value = eval(display.value);
+//     display.innerHTML = ''
+// }
+// function clear() {
+//     display.value = '';
+// }
+// function back() {
+//     display.value = display.value.toString().slice(0, -1);
+// }
