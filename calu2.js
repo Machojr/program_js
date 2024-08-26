de6a70e49ddfb698e8716be7e211df7fbdf37108
kajana1011 <kajanarevocatus@gmail.com>
2024-08-26 20:31:20 +0300

    commitment for commit

--- calu2.js
+++ calu2.js
@@ -1,53 +1,11 @@
 const display = document.getElementById('displayScreen');
 const btnSawasawa = document.getElementById('sawasawa');
+let temporary='';
+let operator='';
+let calculate = '';
 
 const kitufe = document.querySelectorAll('.kitufe');
 console.log(kitufe);
-
-const operate = document.querySelectorAll('.operator');
-console.log(operate);
-operate.forEach(function (operate){
-    console.log(operate);
-    console.log(operate.attributes.value.value);
-    operate.addEventListener('click', function() {
-        const opvalue = operate.attributes.value.value;
-        if (opvalue==='+' || opvalue==='-'|| opvalue==='*' || opvalue==='/') {
-           display.value += opvalue;
-           /* let saveNum = '';
-            let current ='';
-            let op = '';
-
-            function number(kitufeValue) {
-                current +=kitufeValue;
-            }
-            function symbols(opvalue) {
-                if (current==='') {
-                    return;
-                }else {
-                    current=saveNum;
-                    opvalue = op;
-                    current = '';
-                    updateDisplay(op);
-                }
-            }*/
-        }else if(opvalue==='AC') {
-            display.value= '';
-        }else if(opvalue==='C') {
-            display.value=display.value.toString().slice(0, -1);
-        } else if(opvalue==='.') {
-            display.value+= opvalue;
-        } else{
-            try {
-
-                display.value= eval(display.value);
-            }
-            catch(Error) {
-                display.value= 'Syntax Error';
-            }
-        }
-    });
-});
-
 kitufe.forEach(function (kitufe) {
     console.log(kitufe);
     console.log(kitufe.attributes.value.value);
@@ -56,7 +14,12 @@ kitufe.forEach(function (kitufe) {
         if (kitufeValue === 'C' || kitufeValue === 'AC') {
             display.value = '';
         } else if (kitufeValue === '=') {
-            display.value = eval(display.value);
+            calculate = (`${temporary}${operator}${display.value}`);
+            display.value = eval(calculate);
+        }else if(kitufeValue==='*' || kitufeValue==='+'|| kitufeValue==='/' || kitufeValue==='-'){
+            temporary = display.value;
+            display.value = '';
+            operator = kitufeValue;
         } else {
             display.value += kitufeValue;
         }
