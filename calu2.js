cd7daaa6a76799c95f4c0c48b64d3ccce40c5482
Young Rich <tajiriramadhan@gmail.com>
2024-08-26 17:13:48 +0300

    compete arithmetic and clear for the expression

--- calu2.js
+++ calu2.js
@@ -2,6 +2,9 @@ const display = document.getElementById('displayScreen');
 const btnSawasawa = document.getElementById('sawasawa');
 
 const kitufe = document.querySelectorAll('.kitufe');
+let nambaYamwanzo = 0;
+let tendo = '';
+let hesabu = '';
 console.log(kitufe);
 
 kitufe.forEach(function (kitufe) {
@@ -12,7 +15,14 @@ kitufe.forEach(function (kitufe) {
         if (kitufeValue === 'C' || kitufeValue === 'AC') {
             display.value = '';
         } else if (kitufeValue === '=') {
-            display.value = eval(display.value);
+            hesabu = `${nambaYamwanzo}${tendo}${display.value}`
+
+            display.value = eval(hesabu);
+        }
+        else if(kitufeValue == "*" || kitufeValue == "+" || kitufeValue == "-" || kitufeValue == "/"){
+            nambaYamwanzo = display.value;
+            display.value = ""
+            tendo = kitufeValue;
         }
         else {
             display.value += kitufeValue;
